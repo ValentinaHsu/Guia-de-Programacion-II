b82c733accefdd557fd8f710c57c02dfc1908a4e
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-04-21 14:53:40 -0300

    while 01-02

---
+++ Assets/script/while01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class while01 : MonoBehaviour
+{
+    public int encounter = 1, encounterMax = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (encounter < encounterMax){
+            Debug.Log("Este mensaje se repetira,esta en la vuelta numero " + encounter);
+            encounter++;
+        }
+        Debug.Log("Se termino el while");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/while02.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class while02 : MonoBehaviour
+{
+    public int num1, num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 < num2) {
+            while (num1 < num2)
+            {
+                num1++;
+            }   Debug.Log(num1);         
+        }
+        else if (num2 < num1)
+        {
+            while (num2 < num1)
+            {
+                num2++;
+                Debug.Log(num2);
+            }
+            
+        }
+        else 
+        {
+            Debug.Log("Los numeros ingresados son iguales");
+        }
+    }
+}
+
+  
